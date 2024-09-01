2285a13d3cf48383dd7c8570d272db7e81b66d1e
Tillmann Baumeister <tillmbaumeister@gmail.com>
2024-09-01 17:37:51 +0200

    Update testscript2.py

--- testscript2.py
+++ testscript2.py
@@ -1,5 +1,20 @@
 import os, sys
+import tkinter.ttk
 
 print("THis is an Test")
 
 print("Added another textmessage to print")
+
+print("OK, another message")
+
+import tkinter
+print(dir(tkinter))
+
+
+app1 = tkinter.Tk()
+
+TTK = tkinter.ttk
+TTK.Button()
+
+app1.mainloop() 
+
